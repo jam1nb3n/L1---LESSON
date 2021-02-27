c921ac0c72f94e79bc050bbc7084d7c5404d7ae6
Ben Newman <ben@bnewman.dev>
2021-02-27 18:42:04 +0000

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+app.listen(port, () => {
+	console.log(`Server running on port: ${port}`);
+});
